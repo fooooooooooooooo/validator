64b78c6d9cca7f2eb042a75ddf0cdd44aa7c5b98
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2023-05-19 04:32:41 -0700

    dist for github install

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare function parseIpv4(input: string): string | null;
+declare function parseIpv6(input: string): string | null;
+declare function parseIp(input: string): string | null;
+export { parseIpv4, parseIpv6, parseIp };
